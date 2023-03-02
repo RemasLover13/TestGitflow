b7aa459135346d3426b0da2433ca1d63828e6aa7
Edik Krmakov <krmakov2001@mail.ru>
2023-03-02 15:22:32 +0300

    rename method in AuthenticationService

--- TaxiServiceApi/TaxiServiceApi/src/main/java/ru/itis/AuthenticationService.java
+++ TaxiServiceApi/TaxiServiceApi/src/main/java/ru/itis/AuthenticationService.java
@@ -1,7 +1,7 @@
 package ru.itis;
 
 public class AuthenticationService {
-    public void signUp(String email, String password) {
+    public void signIn(String email, String password) {
         valid(email);
         System.out.println(email + "  " + password);
     }
