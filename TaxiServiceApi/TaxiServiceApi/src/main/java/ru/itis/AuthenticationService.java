e11e242b50f41f5a69d749c90fe9ec51dec59758
Edik Krmakov <krmakov2001@mail.ru>
2023-03-02 15:18:59 +0300

    add email validation

--- TaxiServiceApi/TaxiServiceApi/src/main/java/ru/itis/AuthenticationService.java
+++ TaxiServiceApi/TaxiServiceApi/src/main/java/ru/itis/AuthenticationService.java
@@ -2,6 +2,13 @@ package ru.itis;
 
 public class AuthenticationService {
     public void signUp(String email, String password) {
+        valid(email);
         System.out.println(email + "  " + password);
+    }
+
+    private void valid(String email) {
+        if (!email.contains("@")) {
+            throw new IllegalArgumentException("Incorrect email");
+        }
     }
 }
